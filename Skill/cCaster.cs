7527ca25ede3e794fc469aa6cb80fe8994c97ff5
HongJunYeong <red_connec@naver.com>
2017-07-20 15:48:49 +0900

    깃 초기내용

---
+++ Skill/cCaster.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cCaster : MonoBehaviour { 
+
+    #region public 변수
+
+    public GameObject m_objRushCollider;
+    public Canvas m_canvasSkillTree;
+    public GameObject[] m_arraySkill;
+
+    #endregion
+
+
+    #region private 변수
+
+    private GameObject m_objHit;                                 
+
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.K))
+		{
+			m_canvasSkillTree.gameObject.SetActive(!m_canvasSkillTree.gameObject.activeInHierarchy);
+		}
+		SwitchSkillSlot();
+		CastSkill();
+	}
+
+	private void LateUpdate()
+	{
+		
+	}
+
+    #region 스킬 발동 조건 구현
+
+    /// <summary>
+    /// 1, 2, 3의 숫자키를 이용하여 근거리/원거리/보조 스킬 슬롯을 교체한다.
+    /// </summary>
+    public void SwitchSkillSlot()
+	{
+		int nComboStep = -1;
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			nComboStep = cCharacterInformation.Instance.m_nMeleeSkillComboStep;
+            cCharacterInformation.Instance.m_dicCurrentSkillSlot = cCharacterInformation.Instance.m_listDicMeleeSkillSlot[nComboStep];
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			nComboStep = cCharacterInformation.Instance.m_nRangeSkillComboStep;
+            cCharacterInformation.Instance.m_dicCurrentSkillSlot = cCharacterInformation.Instance.m_listDicRangeSkillSlot[nComboStep];
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			nComboStep = cCharacterInformation.Instance.m_nOtherSkillComboStep;
+            cCharacterInformation.Instance.m_dicCurrentSkillSlot = cCharacterInformation.Instance.m_listDicOtherSkillSlot[nComboStep];
+		}
+	}
+
+	/// <summary>
+	/// 좌, 우클릭을 통한 스킬을 선택 및 시전한다.
+	/// </summary>
+	public void CastSkill()
+	{
+		// >> 스킬 선택하는 부분
+		cSkill cCurrentSkill = null;
+		if(Input.GetMouseButtonDown(0))
+		{
+			cCurrentSkill = cCharacterInformation.Instance.m_dicCurrentSkillSlot[Information.eClick.L_CLICK];
+            cCharacterInformation.Instance.m_eClick = Information.eClick.L_CLICK;
+		}
+		else if (Input.GetMouseButtonDown(1))
+		{
+			cCurrentSkill = cCharacterInformation.Instance.m_dicCurrentSkillSlot[Information.eClick.R_CLICK];
+            cCharacterInformation.Instance.m_eClick = Information.eClick.R_CLICK;
+		}
+		// << 
+
+
+		// >> 스킬 시전 가능여부 검사하는 부분
+		if (cCurrentSkill == null) return;
+		if (cCharacterInformation.Instance.m_nChkra < cCurrentSkill.m_cSkillInformation.m_nChakraCost)
+		{
+			Debug.Log("차크라가 부족합니다.");
+			return;
+		}
+		// <<
+
+
+		// >> 스킬 실제 시전하는 부분
+		if(cCurrentSkill.m_cSkillInformation.m_nIdNumber == -1) StartUsingSkill();
+		else
+		{
+			m_arraySkill[cCurrentSkill.m_cSkillInformation.m_nIdNumber].gameObject.SetActive(true);
+		}		
+		// << 
+
+
+
+		// >> 스킬 사용시 공통적으로 변경되는 정보들
+		// 일반공격/스킬사용 버튼 중 일반공격 눌렀을 경우
+		if (cCurrentSkill.m_cSkillInformation.m_nIdNumber == 0 ||
+			cCurrentSkill.m_cSkillInformation.m_nIdNumber == 15 ||
+			cCurrentSkill.m_cSkillInformation.m_nIdNumber == 30)
+		{
+			
+		}
+		// 일반공격/스킬사용 버튼 중 스킬사용 눌렀을 경우
+		else if (cCurrentSkill.m_cSkillInformation.m_nIdNumber == -1)
+		{
+			switch(cCharacterInformation.Instance.m_dicCurrentSkillSlot[Information.eClick.L_CLICK].m_cSkillInformation.m_eType)
+			{
+				case Information.eSkillType.MELEE:
+                    cCharacterInformation.Instance.m_nMeleeSkillComboStep += 1;				
+					break;
+				case Information.eSkillType.RANGE:
+                    cCharacterInformation.Instance.m_nRangeSkillComboStep += 1;
+					break;
+				case Information.eSkillType.OTHER:
+                    cCharacterInformation.Instance.m_nOtherSkillComboStep += 1;
+					break;
+			}
+		}
+		//그 외
+		else
+		{
+			switch (cCurrentSkill.m_cSkillInformation.m_eType)
+			{
+				case Information.eSkillType.MELEE:
+                    cCharacterInformation.Instance.m_nMeleeSkillComboStep += 1;
+					if (cCharacterInformation.Instance.m_nMeleeSkillComboStep > 3) cCharacterInformation.Instance.m_nMeleeSkillComboStep = 0;
+					break;
+				case Information.eSkillType.RANGE:
+                    cCharacterInformation.Instance.m_nRangeSkillComboStep += 1;
+					if (cCharacterInformation.Instance.m_nRangeSkillComboStep > 3) cCharacterInformation.Instance.m_nRangeSkillComboStep = 0;
+					break;
+				case Information.eSkillType.OTHER:
+                    cCharacterInformation.Instance.m_nOtherSkillComboStep += 1;
+					if (cCharacterInformation.Instance.m_nOtherSkillComboStep > 3) cCharacterInformation.Instance.m_nOtherSkillComboStep = 0;
+					break;
+			}
+
+			// 콤보제한시간 안끝났으면 +1
+			if (cCharacterInformation.Instance.m_fComboLimitTime > 0) cCharacterInformation.Instance.m_nTotalComboNum += 1;
+
+            // 스킬 쓴 뒤 콤보제한시간 최대치부터 다시 시작
+            cCharacterInformation.Instance.m_fComboLimitTime = cCharacterInformation.Instance.m_fMaxComboLimitTime;
+		}
+		// <<
+	}
+
+	/// <summary>
+	/// 스킬 사용 시작하도록 함
+	/// </summary>
+	private void StartUsingSkill()
+	{
+
+	}
+
+    #endregion
+
+   
+}
---
+++ Skill/cSkill.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cSkill : MonoBehaviour {
+
+    public cSkillInformation m_cSkillInformation;
+
+    #region protected virtual 함수
+
+    /// <summary>
+    /// 스킬 공통 변수 초기화 메소드
+    /// </summary>
+    /// <param name="skillIndex">스킬 고유번호</param>
+    /// 
+    protected virtual void FirstAwake(int skillIndex)
+    {
+
+        m_cSkillInformation.m_fDamage = cSkillDataBase.Instance.m_dictionarySkillDataBase[skillIndex].m_fDamage;                           //스킬 데미지 (영웅 스탯 비례)
+
+        //캐릭터에서 처리해야함.
+        m_cSkillInformation.m_nChainLevel = cSkillDataBase.Instance.m_dictionarySkillDataBase[skillIndex].m_nChainLevel;                   //스킬 연계 단계
+
+        //나중에 구현 해야함.
+        m_cSkillInformation.m_nDamageType = cSkillDataBase.Instance.m_dictionarySkillDataBase[skillIndex].m_nDamageType;                   //공격 속성      
+        m_cSkillInformation.m_nOptionNumber = cSkillDataBase.Instance.m_dictionarySkillDataBase[skillIndex].m_nOptionNumber;               //옵션 번호
+
+
+        //아직 쓸지 않쓸지 모름
+        //m_eElement = cSkillDataBase.Instance.m_dictionarySkillDataBase[m_nSkillIndex].m_eElement;                       //스킬 속성
+        //m_eType = cSkillDataBase.Instance.m_dictionarySkillDataBase[m_nSkillIndex].m_eType;                             //스킬 타입
+        //m_eAttackRange = cSkillDataBase.Instance.m_dictionarySkillDataBase[m_nSkillIndex].m_eAttackRange;               //스킬 공격 범위
+        //m_nIdNumber = cSkillDataBase.Instance.m_dictionarySkillDataBase[m_nSkillIndex].m_nIdNumber;                     //스킬 고유 번호 
+    }
+
+    protected virtual void GeneralCollision()
+    {
+        //데미지 계산 (총 콤보에 따라 데미지 강해짐.)
+        m_cSkillInformation.m_fDamage = cCharacterInformation.Instance.m_nPhysicalAtk * (cCharacterInformation.Instance.m_nTotalComboNum * 0.05f + 1.0f);
+
+        //데미지 줘야함.
+        //(적 클래스에서 데미지 받는 함수 구현하면 그 함수 불러와서 데미지 줘야함.)
+
+        //캐릭터 상태변화
+        cCharacterInformation.Instance.m_eAnimState = Information.eAnimState.IDLE;
+    }
+
+
+    #endregion
+}
